0e21159c1ab1aa5eac3853a29e139e36c9c2fde0
Sagar <sagarchandgadkar74@gmail.com>
2021-02-15 12:31:39 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,47 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = this.getTime();
+    }
 
-        return(
-            <>
-               
-            </>
-        )
+    componentDidMount() {
+        this.setTimer();
+    }
+
+    setTimer() {
+        clearTimeout(this.timeout);
+        this.timeout = setTimeout(this.updateClock.bind(this), 1000);
+    }
+
+    updateClock() {
+        this.setState(this.getTime, this.setTimer);
+    }
+
+    getTime() {
+        const currentTime = new Date();
+        return {
+            hours: currentTime.getHours(),
+            minutes: currentTime.getMinutes(),
+            seconds: currentTime.getSeconds(),
+            ampm: currentTime.getHours() >= 12 ? 'PM' : 'AM'
+        }
+    }
+
+    render() {
+        const { hours, minutes, seconds, ampm } = this.state;
+        return (
+            <div className="Clock">
+                <h3 id="time">
+                    {hours == 0 ? 12 : hours > 12 ? hours - 12 : hours}:
+                    {minutes > 9 ? minutes : `0${minutes}`}:
+                    {seconds > 9 ? seconds : `0${seconds}`} {ampm}
+                </h3>
+            </div>
+        );
     }
 }
-
 
 export default App;
